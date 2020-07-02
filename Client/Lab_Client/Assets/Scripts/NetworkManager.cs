a0682e5c58651ced84ba691f641aa15b62022067
shun-shun123 <shunsuke15vb@gmail.com>
2020-07-02 18:07:12 +0900

    Clientソースコード作成

---
+++ Client/Lab_Client/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour
+{
+    private const string URL = "http://127.0.0.1:5000";
+    
+    public IEnumerator On()
+    {
+        using (var req = UnityWebRequest.Get(URL + "/on"))
+        {
+            yield return req.SendWebRequest();
+            if (req.isNetworkError || req.isHttpError)
+            {
+                Debug.LogError("NetworkError");
+            }
+        }   
+    }
+
+    public IEnumerator Off()
+    {
+        using (var req = UnityWebRequest.Get(URL + "/off"))
+        {
+            yield return req.SendWebRequest();
+            if (req.isNetworkError || req.isHttpError)
+            {
+                Debug.LogError("NetworkError");
+            }
+        }
+    }
+}
---
+++ Client/Lab_Client/Assets/Scripts/UserInputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UserInputManager : MonoBehaviour
+{
+
+    [SerializeField] private NetworkManager m_NetworkManager;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(m_NetworkManager.On());
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            StartCoroutine(m_NetworkManager.Off());
+        }
+    }
+}
