190ef37759d6c13d1bb525cb6c4a53761b68b980
shun-shun123 <shunsuke15vb@gmail.com>
2020-07-06 11:45:45 +0900

    TestApp作成

---
+++ TestApp/Assets/Scripts/UserInputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInputManager : MonoBehaviour
+{
+    [SerializeField] private Button countButton;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private Text countText;
+
+    private int _count;
+
+    private void Start()
+    {
+        Bind();   
+    }
+
+    private void Bind()
+    {
+        countButton.onClick.AddListener(CountUp);
+        resetButton.onClick.AddListener(Reset);
+    }
+
+    private void CountUp()
+    {
+        _count++;
+        countText.text = _count.ToString();
+    }
+
+    private void Reset()
+    {
+        _count = 0;
+        countText.text = _count.ToString();
+    }
+}
