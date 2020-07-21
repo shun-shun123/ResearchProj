1cd877936be1e7864f016c0ed71438e3d75f42fc
shun-shun123 <shunsuke15vb@gmail.com>
2020-07-22 08:43:46 +0900

    シリアルポートに繋ぐかをコマンドライン引数から選択可能にした

--- Server/server.py
+++ Server/server.py
@@ -1,27 +1,40 @@
 from flask import Flask, request
 import serial
+import sys
+
 app = Flask(__name__)
 
-ser = serial.Serial()
-ser.port = "/dev/cu.usbmodem143301"
-ser.baudrate = 9600
-ser.setDTR(False)
-ser.open()
+is_use_serial_port = True
+
+if len(sys.argv) > 1:
+    is_use_serial_port = int(sys.argv[1]) == 0
+
+if is_use_serial_port:
+    ser = serial.Serial()
+    ser.port = "/dev/cu.usbmodem143301"
+    ser.baudrate = 9600
+    ser.setDTR(False)
+    ser.open()
 
 @app.route('/on')
 def serial_on():
-    ser.write(b'1')
+    if is_use_serial_port:
+        ser.write(b'1')
     return "ON"
 
 @app.route('/off')
 def serial_off():
-    ser.write(b'0')
+    if is_use_serial_port:
+        ser.write(b'0')
     return "OFF"
 
 @app.route('/command')
 def recieve_command():
     command = request.args.get("cmd")
-    ser.write(command.encode())
+    if is_use_serial_port:
+        ser.write(command.encode())
+    else:
+        print(command)
     return command.encode()
 
 if __name__ == "__main__":
