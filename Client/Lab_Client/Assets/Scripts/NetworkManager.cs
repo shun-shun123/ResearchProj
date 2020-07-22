2392c5050d823eaf7e2e53bf8ba0b244d2d3903e
shun-shun123 <shunsuke15vb@gmail.com>
2020-07-22 11:08:53 +0900

    5種類のタッチを検出可能にした

--- Client/Lab_Client/Assets/Scripts/NetworkManager.cs
+++ Client/Lab_Client/Assets/Scripts/NetworkManager.cs
@@ -1,10 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviour
 {
+    [SerializeField] private Text m_TextLog;
+    
+#if UNITY_EDITOR
     private const string URL = "http://127.0.0.1:5000";
+#else
+    private const string URL = "http://192.168.10.7:5000";
+#endif
+    
+
+    private string m_ReceivedCommand;
 
     public IEnumerator On()
     {
@@ -30,8 +43,47 @@ public class NetworkManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        Observable
+            .Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1))
+            .Where(_ => string.IsNullOrEmpty(m_ReceivedCommand) == false)
+            .Subscribe(_ =>
+            {
+                StartCoroutine(Command(m_ReceivedCommand));
+                switch (m_ReceivedCommand[0])
+                {
+                    case '1':
+                        m_TextLog.text = "SingleTouch";
+                        break;
+                    case '2':
+                        m_TextLog.text = "DoubleTouch";
+                        break;
+                    case '3':
+                        m_TextLog.text = "Hold";
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        m_TextLog.text = "Scroll";
+                        break;
+                    case '6':
+                        m_TextLog.text = "Pinch-In";
+                        break;
+                    case '7':
+                        m_TextLog.text = "Pinch-Out";
+                        break;
+                    default:
+                        m_TextLog.text = "Not Found";
+                        break;
+                }
+                m_ReceivedCommand = null;
+            }).AddTo(gameObject);
+    }
+
     public IEnumerator Command(string cmd)
     {
+        Debug.Log($"Network.Command: {cmd[0]}");
         using (var req = UnityWebRequest.Get(URL + "/command?cmd=" + cmd))
         {
             yield return req.SendWebRequest();
@@ -41,4 +93,10 @@ public class NetworkManager : MonoBehaviour
             }
         }
     }
+
+    /// <summary>
+    /// 外部からコマンドをセットすることができる
+    /// </summary>
+    /// <param name="cmd"></param>
+    public void SetCommand(string cmd) => m_ReceivedCommand = cmd;
 }
--- Client/Lab_Client/Assets/Scripts/UserInputManager.cs
+++ Client/Lab_Client/Assets/Scripts/UserInputManager.cs
@@ -8,13 +8,35 @@ public class UserInputManager : MonoBehaviour
 
     [SerializeField] private NetworkManager m_NetworkManager;
 
+    [Header("ログに表示したい内容")] 
+    [SerializeField] private bool ShowOnPointerDownAsObservable = true;
+    [SerializeField] private bool ShowSingleTouchObservable = true;
+    [SerializeField] private bool ShowDoubleTouchObservable = true;
+    [SerializeField] private bool ShowHoldObservable = true;
+    [SerializeField] private bool ShowDragObservable = true;
+    [SerializeField] private bool ShowPinchInObservable = true;
+
     private TouchInfo m_TouchInfo = new TouchInfo();
 
     public static float WIDTH => Screen.width;
     
     public static float HEIGHT => Screen.height;
 
-    private const float FAST_TOUCH_SEC = 0.2f;
+    private const float DOUBLE_TOUCH_DURATION = 0.2f;
+
+    private const float HOLD_DURATION = 0.2f;
+
+    private float m_LastReleaseTime;
+    
+    private TouchInfo m_DragStartInfo = new TouchInfo();
+
+    private Vector2 m_PinchInStartTouch1;
+    private Vector2 m_PinchInStartTouch2;
+    private Vector2 m_PinchInEndTouch1;
+    private Vector2 m_PinchInEndTouch2;
+
+    private Vector2 m_PinchOutStart;
+    private Vector2 m_PinchOutEnd;
     
 
     public struct TouchInfo
@@ -43,7 +65,10 @@ public class UserInputManager : MonoBehaviour
             {
                 m_TouchInfo.SetStartPosition(data.position);
                 m_TouchInfo.StartTime = Time.time;
-                Debug.Log($"Touch Start. Point at {m_TouchInfo.StartPoint}");
+                if (ShowOnPointerDownAsObservable)
+                {
+                    Debug.Log($"Touch Start. Point at {m_TouchInfo.StartPoint}");
+                }
             });
         
         // 各種タッチ処理のイベント登録
@@ -62,31 +87,83 @@ public class UserInputManager : MonoBehaviour
         trigger.OnPointerUpAsObservable()
             .Where(data =>
             {
-                var screenAspectPoint = new Vector2(data.position.x / WIDTH, data.position.y / HEIGHT);
-                // 条件1: タッチ開始地点から動いていないか
-                bool isNotMoved = Vector2.Distance(screenAspectPoint, m_TouchInfo.StartPoint) <= 0.1f;
-
-                // 条件2: タッチ開始からの経過時間が0.2sec以下か
-                bool isFastTouch = (Time.time - m_TouchInfo.StartTime) <= FAST_TOUCH_SEC;
-
-                return isNotMoved && isFastTouch;
+                // 前回指を離した時間から0.2sec以下ならダブルクリックとしてSingleTouchは発火しない
+                if (Time.time - m_LastReleaseTime <= DOUBLE_TOUCH_DURATION || 
+                    Time.time - m_TouchInfo.StartTime >= DOUBLE_TOUCH_DURATION)
+                {
+                    return false;
+                }
+                m_LastReleaseTime = Time.time;
+                return true;
             })
             .Subscribe(data =>
             {
                 // TODO: SingleTouch時の処理
                 var uv = CalcScreenUv(data.position);
-                StartCoroutine(m_NetworkManager.Command($"1,{uv.x},{uv.y}"));
-                Debug.Log($"SingleTouch: {CalcScreenUv(data.position)}");
+                m_NetworkManager.SetCommand($"1,{uv.x},{uv.y}");
+                if (ShowSingleTouchObservable)
+                {
+                    Debug.Log($"SingleTouch: {uv}");
+                }
             }).AddTo(gameObject);
     }
 
     private void DoubleTouch(ObservableEventTrigger trigger)
     {
+        // 実装参考URL
+        // https://qiita.com/miyaoka/items/6a643434a3f1548fb489
+        var tapStream = Observable.EveryUpdate().Where(_ =>
+        {
+            if (Input.touchCount > 0)
+            {
+                return Input.GetTouch(0).phase == TouchPhase.Began;
+            }
+            return false;
+        });
+        tapStream.TimeInterval()
+            .Select(t => t.Interval.TotalMilliseconds)
+            .Buffer(2, 1)
+            .Where(list => list[0] > 200)
+            .Where(list => list[1] <= 200)
+            .Where(_ => Input.touchCount == 1)
+            .Select(_ => Input.GetTouch(0))
+            .Subscribe(touch =>
+            {
+                var uv = CalcScreenUv(touch.position);
+                m_NetworkManager.SetCommand($"2,{uv.x},{uv.y},0.2");
+                if (ShowDoubleTouchObservable)
+                {
+                    Debug.Log($"DoubleTouch: {uv}");
+                }
+            }).AddTo(gameObject);
     }
 
     private void Hold(ObservableEventTrigger trigger)
     {
-        
+        this.UpdateAsObservable()
+            .SkipUntil(trigger.OnPointerDownAsObservable())
+            .TakeUntil(trigger.OnPointerUpAsObservable()
+                .Do(_ =>
+                {
+                    if (Time.time - m_TouchInfo.StartTime >= HOLD_DURATION)
+                    {
+                        var uv = m_TouchInfo.StartPoint;
+                        m_NetworkManager.SetCommand($"3,{uv.x},{uv.y},0.5");
+                        if (ShowHoldObservable)
+                        {
+                            Debug.Log($"Hold: ");
+                        }
+                    }
+                })
+            )
+            .RepeatUntilDestroy(this)
+            .Subscribe(_ =>
+            {
+                if (ShowHoldObservable)
+                {
+                    Debug.Log("Holding...");
+                }
+            }).AddTo(gameObject);
     }
 
     private void PressAndTouch(ObservableEventTrigger trigger)
@@ -96,12 +173,93 @@ public class UserInputManager : MonoBehaviour
 
     private void Scroll(ObservableEventTrigger trigger)
     {
+        trigger.OnPointerDownAsObservable()
+            .Subscribe(data =>
+            {
+                m_DragStartInfo.SetStartPosition(data.position);
+                m_DragStartInfo.StartTime = Time.time;
+            }).AddTo(gameObject);
         
+        this.UpdateAsObservable()
+            .SkipUntil(trigger.OnBeginDragAsObservable())
+            .TakeUntil(trigger.OnEndDragAsObservable().Do(data =>
+            {
+                // 二本指以上でのスクロールはピンチイン・アウトの可能性があるから
+                if (Input.touchCount < 2)
+                {
+                    var startUv = m_DragStartInfo.StartPoint;
+                    var endUv = CalcScreenUv(data.position);
+                    m_NetworkManager.SetCommand($"5,{startUv.x},{startUv.y},{endUv.x},{endUv.y}");
+                    if (ShowDragObservable)
+                    {
+                        Debug.Log($"Scroll: 5,{startUv.x},{startUv.y},{endUv.x},{endUv.y}");
+                    }
+                }
+            }))
+            .RepeatUntilDestroy(this)
+            .Subscribe(_ =>
+            {
+                if (ShowDragObservable)
+                {
+                    Debug.Log("Scrolling...");
+                }
+            }).AddTo(gameObject);
     }
 
     private void PinchIn(ObservableEventTrigger trigger)
     {
-        
+        this.UpdateAsObservable()
+            .Where(_ => Input.touchCount >= 2)
+            .SkipUntil(trigger.OnPointerDownAsObservable().Do(_ =>
+            {
+                if (Input.touchCount >= 2)
+                {
+                    var touch1 = Input.GetTouch(0);
+                    var touch2 = Input.GetTouch(1);
+                    m_PinchInStartTouch1 = CalcScreenUv(touch1.position);
+                    m_PinchInStartTouch2 = CalcScreenUv(touch2.position);
+                }
+            }))
+            .TakeUntil(trigger.OnPointerUpAsObservable().Do(_ =>
+            {
+                if (Input.touchCount >= 2)
+                {
+                    var touch1 = Input.GetTouch(0);
+                    var touch2 = Input.GetTouch(1);
+                    m_PinchInEndTouch1 = CalcScreenUv(touch1.position);
+                    m_PinchInEndTouch2 = CalcScreenUv(touch2.position);
+                    // ピンチイン開始のタッチ指間の距離 < ピンチイン終了時のタッチ指間の距離
+                    // がそもそもピンチインの定義
+                    var startDist = Vector2.Distance(m_PinchInStartTouch1, m_PinchInStartTouch2);
+                    var endDist = Vector2.Distance(m_PinchInEndTouch1, m_PinchInEndTouch2);
+                    if (startDist < endDist)
+                    {
+                        var position = (m_PinchInStartTouch1 + m_PinchInStartTouch2) / 2.0f;
+                        m_NetworkManager.SetCommand($"6,{position.x},{position.y},{endDist - startDist}");
+                        if (ShowPinchInObservable)
+                        {
+                            Debug.Log($"PinchIn: 6,{position.x},{position.y},{endDist - startDist}");
+                        }
+                    }
+                    else
+                    {
+                        var position = (m_PinchInStartTouch1 + m_PinchInStartTouch2) / 2.0f;
+                        m_NetworkManager.SetCommand($"7,{position.x},{position.y},{startDist - endDist}");
+                        if (ShowPinchInObservable)
+                        {
+                            Debug.Log($"PinchOut: 7,{position.x},{position.y},{startDist - endDist}");
+                        }
+                    }
+                }
+            }))
+            .RepeatUntilDestroy(this)
+            .Subscribe(_ =>
+            {
+                if (ShowPinchInObservable)
+                {
+                    Debug.Log("ピンチイン中");
+                }
+            }).AddTo(gameObject);
     }
 
     private void PinchOut(ObservableEventTrigger trigger)
