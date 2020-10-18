a4563732c1e3a3da8fd70707c009ae881029e6dc
shun-shun123 <shunsuke15vb@gmail.com>
2020-10-18 18:04:54 +0900

    コード整理

---
+++ Client/Lab_Client/Assets/Scripts/SceneChangeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour
+{
+    public void GoToTestScene()
+    {
+        SceneManager.LoadScene("TestSendTouchDataAccuracy");
+    }
+
+    public void GoToSampleScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GoToBitReceiveScene()
+    {
+        SceneManager.LoadScene("BitReceive");
+    }
+
+    public void GoToTouchDurationRapidTestScene()
+    {
+        SceneManager.LoadScene("TouchDurationRapidTest");
+    }
+}
