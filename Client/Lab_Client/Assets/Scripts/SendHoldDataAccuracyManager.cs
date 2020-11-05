64caf3a77bd106bd4c9afb81e302545807914858
shun-shun123 <shunsuke15vb@gmail.com>
2020-11-05 17:16:25 +0900

    ホールドデータにして送信するようにした

---
+++ Client/Lab_Client/Assets/Scripts/SendHoldDataAccuracyManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SendHoldDataAccuracyManager : MonoBehaviour
+{
+    [SerializeField] private HoldEventReceiver holdEventReceiver;
+
+    [Header("Test Parameters")] 
+    [SerializeField] private float holdDuration;
+    [SerializeField] private float testMaxCount;
+    
+    private float lastTime;
+
+    private readonly int[] _bitData = new int[10];
+
+    /// <summary>
+    /// データの受信中フラグ
+    /// </summary>
+    private bool _isDataReceiving;
+
+    private void Start()
+    {
+        holdEventReceiver.OnPointerDownAction = OnPointerDown;
+        holdEventReceiver.OnPointerUpAction = OnPointerUp;
+    }
+
+    private void OnPointerDown(PointerEventData data)
+    {
+        lastTime = Time.time;
+        if (_isDataReceiving)
+        {
+            // TODO: データ受信中の処理
+        }
+        else
+        {
+            _isDataReceiving = true;
+        }
+    }
+
+    private void OnPointerUp(PointerEventData data)
+    {
+        Debug.Log($"Difference: {Time.time - lastTime}");
+    }
+}
---
+++ Client/Lab_Client/Assets/Scripts/UI/HoldEventReceiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoldEventReceiver : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Action<PointerEventData> OnPointerDownAction;
+
+    public Action<PointerEventData> OnPointerUpAction;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnPointerDownAction?.Invoke(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnPointerUpAction?.Invoke(eventData);
+    }
+}
