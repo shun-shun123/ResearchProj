b68755d0d9e2c56dbc3a1ed2d259f6123da26d00
shun-shun123 <shunsuke15vb@gmail.com>
2020-11-12 00:49:48 +0900

    ホールド時間の精度をテストするコードの実装

---
+++ Client/Lab_Client/Assets/Scripts/HoldDuationAccTestManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoldDuationAccTestManager : MonoBehaviour
+{
+    [SerializeField]
+    private int testMaxCount;
+
+    [SerializeField]
+    private float holdDuration;
+
+    [SerializeField]
+    private HoldEventReceiver holdEventReceiver;
+
+    private float _holdDuration;
+
+    private int _testCount;
+
+    private float _totalDiff;
+    
+    private void Start()
+    {
+        holdEventReceiver.OnPointerDownAction = OnPointerDown;
+        holdEventReceiver.OnPointerUpAction = OnPointerUp;
+    }
+
+
+    private void OnPointerDown(PointerEventData data)
+    {
+        _holdDuration = Time.realtimeSinceStartup;
+    }
+
+    private void OnPointerUp(PointerEventData data)
+    {
+        _totalDiff = holdDuration - (Time.realtimeSinceStartup - _holdDuration);
+        _testCount++;
+        if (_testCount >= testMaxCount)
+        {
+            _totalDiff /= (float) _testCount;
+            Debug.Log($"Diff.Ave: {_totalDiff}");
+            _testCount = 0;
+        }
+    }
+}
