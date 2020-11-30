bf3417337f143bb170f5caf9280d11471613b66e
shun-shun123 <shunsuke15vb@gmail.com>
2020-12-01 03:12:12 +0900

    平均値を出力できるようにした

--- Client/Lab_Client/Assets/Scripts/HoldDurationAccTestManager.cs
+++ Client/Lab_Client/Assets/Scripts/HoldDurationAccTestManager.cs
@@ -8,16 +8,31 @@ using UnityEngine.EventSystems;
 
 public class HoldDurationAccTestManager : MonoBehaviour
 {
+    /// <summary>
+    /// 一度で行うテスト回数
+    /// </summary>
+    [SerializeField]
+    private int testCount;
+
+    /// <summary>
+    /// ホールド時間（理想）
+    /// </summary>
+    [SerializeField]
+    private int textHoldDuration;
+    
     [SerializeField]
     private HoldEventReceiver holdEventReceiver;
 
+    /// <summary>
+    /// 別スレッドで走らせるタイマーのTick
+    /// </summary>
     [SerializeField]
     private double threadTimerInMillis;
 
     /// <summary>
     /// 非同期でカウントするタイマー
     /// </summary>
-    private double _threadTimer;
+    private int _threadTimer;
 
     /// <summary>
     /// タイマーロック
@@ -28,6 +43,16 @@ public class HoldDurationAccTestManager : MonoBehaviour
     /// マルチスレッドブロック
     /// </summary>
     private readonly object _lockObj = new object();
+
+    /// <summary>
+    /// 現在の試行回数
+    /// </summary>
+    private int _currentTestIter;
+
+    /// <summary>
+    /// タイマーの合計（平均を出力するために使う）
+    /// </summary>
+    private int _totalTimer;
     
     private void Start()
     {
@@ -52,15 +77,24 @@ public class HoldDurationAccTestManager : MonoBehaviour
                 }
                 lock (_lockObj)
                 {
-                    _threadTimer += threadTimerInMillis;
+                    _threadTimer += (int)threadTimerInMillis;
                 }
             }).AddTo(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _currentTestIter = 0;
+        }
     }
 
     private void OnPointerDown(PointerEventData data)
     {
         // タイマーロックを解除してカウンターを0にする
         _timerLock = false;
+        _currentTestIter++;
         lock (_lockObj)
         {
             _threadTimer = 0;
@@ -70,6 +104,11 @@ public class HoldDurationAccTestManager : MonoBehaviour
     private void OnPointerUp(PointerEventData data)
     {
         _timerLock = true;
-        Debug.Log($"HoldTime: {_threadTimer}");
+        _totalTimer += _threadTimer;
+        Debug.Log($"HoldDuration: {_threadTimer}");
+        if (_currentTestIter == testCount)
+        {
+            Debug.Log($"Average: {_totalTimer / (float)testCount}");
+        }
     }
 }
